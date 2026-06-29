2530abe68f00afe211188170434ebbb1a25589db
Rakesh07778777 <bossrakesh43@gmail.com>
2026-06-30 00:06:14 +0530

    first commit cpp

---
+++ index.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int main(){
+    //endl will flush the buffer
+    // std::cout << "Hey sup" << std::endl ;
+    // std::cout << "Nothing much" << std::endl ;
+    //new line(\n) character both are same..
+    std::cout << "Hey sup" << '\n';
+    std::cout << "Nothing much" << '\n';
+    return 0;
+}
