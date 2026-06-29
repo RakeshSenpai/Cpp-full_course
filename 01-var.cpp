82e267ddf8c04af48ea93e605939fa4f34bb155c
Rakesh07778777 <bossrakesh43@gmail.com>
2026-06-30 00:27:52 +0530

    variable cpp

---
+++ 01-var.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main(){
+    int x;//declaration
+    x = 5;//assignment
+    // int x = 5;
+
+    int y = 4;
+    int sum = x + y;
+    // int deci = 7.5;
+    
+    std::cout << x << '\n';
+    std::cout << y << '\n';
+    std::cout << sum << '\n';
+    // std::cout << deci << '\n';
+
+    //Double (number including decimals)
+    
+    double gpa = 6.5;
+
+    std::cout << gpa;
+
+    return 0;
+}
