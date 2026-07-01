99a4bc2cedf69e655a64845115ac95e5e2e42691
Rakesh07778777 <bossrakesh43@gmail.com>
2026-07-01 23:29:35 +0530

    arithemetic operators

---
+++ 05-A-operators.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int a = 10;
+    int b = 2;
+    cout << "Addition = " << a + b << endl;
+    cout << "Substraction = " << a - b << endl;
+    cout << "Multiplication = " << a * b << endl;
+    cout << "Divison = " << a / b << endl;
+    cout << "Modulus = " << a % b << endl;
+
+    return 0;
+};
