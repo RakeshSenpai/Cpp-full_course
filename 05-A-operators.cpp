b7cd8b0bff8a48d545202afa1e38822641a9d84a
Rakesh07778777 <bossrakesh43@gmail.com>
2026-07-01 23:35:43 +0530

    arithmetic operators

--- 05-A-operators.cpp
+++ 05-A-operators.cpp
@@ -12,5 +12,17 @@ int main(){
     cout << "Divison = " << a / b << endl;
     cout << "Modulus = " << a % b << endl;
 
+    //Increment and decrement
+
+    int x = 5;
+    cout << "x: " << x << endl;
+    cout << "x++: " << x++ << endl;
+    cout << "After x++: " << x << endl;
+    cout << "++x: " << ++x << endl;
+    cout << "After ++x: " << x << endl;
+    cout << "x--: " << x-- << endl;
+    cout << "After x--: " << x << endl;
+    cout << "--x: " << --x << endl;
+    cout << "After --x: " << x << endl;
     return 0;
 };
