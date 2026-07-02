d530d3f62f3b85ce4e24e0c33ab830629358332d
Rakesh07778777 <bossrakesh43@gmail.com>
2026-07-02 23:48:51 +0530

    type conversation

--- 06-type-convo.cpp
+++ 06-type-convo.cpp
@@ -4,8 +4,17 @@ using namespace std;
 
 int main(){
 
-    double x = (int) 3.14;
-    cout << x;
+    //type conversation -- conversation value of one data type to another
+    // implicit -- automatic
+    // explicit -- precede value with new data type (int)
+
+
+    // double x = (int) 3.14;
+    // cout << x;
+    int question = 10;
+    int correct = 8;
+    double score = correct/static_cast<double>(question) * 100;
+    cout << score << "%";
 
     return 0;
 }
