252a48c147dd6f8bdcfec7f24b687fe8a536fc79
Rakesh07778777 <bossrakesh43@gmail.com>
2026-07-06 23:36:40 +0530

    user input in cpp

---
+++ 07-user-input.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(){
+    // out << insertion operator
+    //cin >> extractor operator
+
+    std::string name;
+    std::cout << "What's your name?: ";
+    std::cin >> name;
+    std::cout << "Hello " << name;
+
+
+
+    return 0;
+    
+};
