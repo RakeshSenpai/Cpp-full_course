4e5ef787e700d3b642be22c45376224f4381f7ef
Rakesh07778777 <bossrakesh43@gmail.com>
2026-07-06 23:40:18 +0530

    user input

--- 07-user-input.cpp
+++ 07-user-input.cpp
@@ -5,12 +5,18 @@ int main(){
     //cin >> extractor operator
 
     std::string name;
+    int age ;
     std::cout << "What's your name?: ";
     std::cin >> name;
-    std::cout << "Hello " << name;
+
+    std::cout << "What's your Age?: ";
+    std::cin >> age;
+
+    std::cout << "Hello " << name << '\n';
+    std::cout << "Your age is " << age;
 
 
 
-    return 0;
+    return 0; 
     
 };
