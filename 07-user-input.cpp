eb72637356e93ce4fca706d4651d1ee2e18c5670
Rakesh07778777 <bossrakesh43@gmail.com>
2026-08-18 23:45:49 +0530

    user-input

--- 07-user-input.cpp
+++ 07-user-input.cpp
@@ -6,11 +6,13 @@ int main(){
 
     std::string name;
     int age ;
-    std::cout << "What's your name?: ";
-    std::getline(std::cin, name);
 
     std::cout << "What's your Age?: ";
     std::cin >> age;
+
+    std::cout << "What's your name?: ";
+    std::getline(std::cin >>std::ws, name);
+
 
     std::cout << "Hello " << name << '\n';
     std::cout << "Your age is " << age;
