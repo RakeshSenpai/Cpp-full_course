b0bffb81bb17b2a4bba57eb66fb5f66eff45087e
Rakesh07778777 <bossrakesh43@gmail.com>
2026-08-28 23:48:08 +0530

    minmax func

--- 08.maths-fun.cpp
+++ 08.maths-fun.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 int main(){
 
@@ -7,7 +8,9 @@ int main(){
     double z;
 
     //Maax funciton
-     z = std::max(x, y);
+    z = pow(2, 3);
+    //  z = std::max(x, y);
+    std::cout << z;
 
 
     return 0;
