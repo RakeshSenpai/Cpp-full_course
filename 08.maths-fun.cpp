b4ad17d5bbe293158a9e710eb5b46b0a68620b38
Rakesh07778777 <bossrakesh43@gmail.com>
2026-08-31 23:54:59 +0530

    maths functions

--- 08.maths-fun.cpp
+++ 08.maths-fun.cpp
@@ -3,15 +3,21 @@
 
 int main(){
 
-    double x = 3;
+    double x = 3.15;
     double y = 4;
     double z;
 
     //Maax funciton
     z = pow(2, 3);
     //  z = std::max(x, y);
+    // z = std::min(x,y);
+    //    z = std::pow(2,5);
+    // z = std::sqrt(81);
+    // z = std::abs(-10);
+    // z = std::round(x);
+    // z = std::ceil(x); 
+    z = std::floor(x);
     std::cout << z;
-
 
     return 0;
 }
