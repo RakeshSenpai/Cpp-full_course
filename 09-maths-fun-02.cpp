40f0204b0bde2aec97adeb4f67c1a7083e8dedad
Rakesh07778777 <bossrakesh43@gmail.com>
2026-09-01 23:56:10 +0530

    maths calculate

---
+++ 09-maths-fun-02.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+int main(){
+
+    double a;
+    double b;
+    double c;
+
+    std::cout << "Enter side A: ";
+    std::cin >> a;
+    std::cout << "Enter side B: ";
+    std::cin >> b;
+
+    
+
+    return 0;
+}
