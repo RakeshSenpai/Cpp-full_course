90e5ad9f987fe235a31280dc97a83643737cb82a
Rakesh07778777 <bossrakesh43@gmail.com>
2026-09-10 23:44:16 +0530

    maths

--- 09-maths-fun-02.cpp
+++ 09-maths-fun-02.cpp
@@ -18,5 +18,12 @@ int main(){
 
     std::cout << "Side C :" << c;
 
+    double x = 5.7;
+
+std::cout << ceil(a);   // 6
+std::cout << floor(a);  // 5
+std::cout << round(a);  // 6
+std::cout << trunc(a);  // 5
+
     return 0;
 }
